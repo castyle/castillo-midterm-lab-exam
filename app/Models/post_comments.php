a5fb8ce97839a0dfa8107c1e45e6e358eb049b70
castyle <castilloeb.569.stud@cdd.edu.ph>
2024-07-18 11:25:27 +0800

    initial commit

---
+++ app/Models/post_comments.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+use App\models\post_comments;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post_comments extends Model
+{
+    use HasFactory;
+}
