635fad798729d9d3bd8ebaa807c67a712c8f23be
castyle <castilloeb.569.stud@cdd.edu.ph>
2024-07-18 12:06:41 +0800

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
+use App\Http\Controllers\comment_replies;
+use App\Http\Controllers\post_comments;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('comment_replies' , comment_repliesController::class  );
+Route::resource('post_comments' , post_commentsController::class);
